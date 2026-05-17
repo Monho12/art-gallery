272b4376bc590ecddcd10281e66ee203da57b296
Monho12 <oorgilmojo@gmail.com>
2026-05-17 20:44:58 +0800

    backend

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.nextTick.PORT || 5001
+
+
+
+app.use(express.json())
+app.use(rateLimiter)
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("server started on PORT:", PORT);
+    });
+});
+
+
