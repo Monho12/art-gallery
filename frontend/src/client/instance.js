2cbd5ada6da9f0db652aca2c9d43fa0f79fb7718
Monho12 <oorgilmojo@gmai.com>
2026-06-05 11:02:20 +0800

    frontend

--- frontend/src/client/instance.js
+++ frontend/src/client/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "https://art-be.vercel.app",
+    // baseURL: "http://localhost:5001",
 })
 
 instance.interceptors.request.use((config) => {
